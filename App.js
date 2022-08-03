da2fe2f3023c6e49bdecb5afbc619aff7654bc8a
rogerzuchu21022019 <97218806+rogerzuchu21022019@users.noreply.github.com>
2022-08-03 21:45:53 +0700

    create project

---
+++ App.js
@@ -0,0 +1,32 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>HEHE!</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <Text>HAHA</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#aaa',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
